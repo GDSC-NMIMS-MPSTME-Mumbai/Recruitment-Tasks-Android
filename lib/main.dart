665f1b4e15dab047e5f35694184929b8826365d6
ShounakDeshmukh <shounsach@gmail.com>
2021-09-21 00:25:39 +0530

    Api working

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'model/json_model.dart';
-import 'network/network_service.dart';
+import 'screens/home_page.dart';
 
 void main() {
   runApp(WeatherApp());
@@ -13,49 +12,10 @@ class WeatherApp extends StatefulWidget {
 }
 
 class _WeatherAppState extends State<WeatherApp> {
-  final _cityTextController = TextEditingController();
-  final _dataService = DataService();
-  WeatherResponse _response;
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // ignore: unnecessary_null_comparison
-            if (_response != null)
-              Column(
-                children: [
-                  Image.network(_response.iconUrl),
-                  Text(
-                    '${_response.tempInfo.temperature}°',
-                    style: TextStyle(fontSize: 40),
-                  ),
-                  Text(_response.weatherInfo.description)
-                ],
-              ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 50),
-              child: SizedBox(
-                width: 150,
-                child: TextField(
-                    controller: _cityTextController,
-                    decoration: InputDecoration(labelText: 'City'),
-                    textAlign: TextAlign.center),
-              ),
-            ),
-            ElevatedButton(onPressed: _search, child: Text('Search'))
-          ],
-        ),
-      ),
-    ));
-  }
-
-  void _search() async {
-    final response = await _dataService.getWeather(_cityTextController.text);
-    setState(() => _response = response);
+      home: HomePage(),
+    );
   }
 }
